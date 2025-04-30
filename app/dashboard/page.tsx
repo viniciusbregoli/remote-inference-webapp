63c4fe444fb0080c9731ee11620c2fefce1bb884
Vinicius Bregoli <vinibregoli@gmail.com>
2025-04-30 16:16:33 +0200

    Fix padding

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -67,7 +67,7 @@ export default function Dashboard() {
 
   // Admin dashboard
   return (
-    <div className="max-w-7xl mx-auto">
+    <div className="max-w-7xl mx-auto px-4">
       <div className="mb-8">
         <h1 className="text-3xl md:text-4xl font-bold text-gray-800">
           Admin Dashboard
